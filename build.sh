cd718eec77e549e9cfa9e46aa80260cc01da9097
David Christenson <david@the-david.com>
2017-02-15 21:27:08 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -z "$TOOLCHAIN_DIR" ]]; then
+	TOOLCHAIN_DIR=/toolchain
+fi
+
+INFRASTRUCTURE=ftp://gcc.gnu.org/pub/gcc/infrastructure/
+
+GMP_VER=6.1.2
+ISL_VER=0.16.1
+MPC_VER=1.0.3
+MPFR_VER=3.1.4
+GCC_VER=6.3.0
+OPENSSL_VER=1.0.2k
+
+mkdir "$TOOLCHAIN_DIR"
+
+export PATH="${TOOLCHAIN_DIR}/bin:${PATH}"
+export LD_LIBRARY_PATH="${TOOLCHAIN_DIR}/lib64:${TOOLCHAIN_DIR}/lib:${LD_LIBRARY_PATH}"
+export CFLAGS="-I${TOOLCHAIN_DIR}/include"
+export CXXFLAGS="-I${TOOLCHAIN_DIR}/include"
+
+
+echo "Building GMP"
+FILE="gmp-${GMP_VER}.tar.bz2"
+curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+tar -xf "$FILE"
+rm "$FILE"
+cd "gmp-${GMP_VER}"
+./configure --prefix="$TOOLCHAIN_DIR" --enable-cxx
+make
+make check
+make install
+cd ..
+
+echo "Building MPFR"
+FILE="mpfr-${GMP_VER}.tar.bz2"
+curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+tar -xf "$FILE"
+rm "$FILE"
+cd "mpfr-${MPF_VER}"
+./configure --disable-dependency-tracking --prefix="$TOOLCHAIN_DIR" --disable-silent-rules
+make
+make check
+make install
+cd ..
+
+echo "Building MPC"
+FILE="mpc-${GMP_VER}.tar.gz"
+curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+tar -xf "$FILE"
+rm "$FILE"
+cd "mpc-${MPC_VER}"
+./configure --prefix="$TOOLCHAIN_DIR" --disable-dependency-tracking --with-gmp="$TOOLCHAIN_DIR" --with-mpfr="$TOOLCHAIN_DIR"
+make
+make check
+make install
+cd ..
+
+
+echo "Building ISL"
+FILE="isl-${GMP_VER}.tar.bz2"
+curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+tar -xf "$FILE"
+rm "$FILE"
+cd "isl-${ISL_VER}"
+./configure --disable-dependency-tracking --disable-silent-rules --prefix="$TOOLCHAIN_DIR" --with-gmp=system --with-gmp-prefix="$TOOLCHAIN_DIR"
+make check
+make install
+
+echo "Building GCC"
+FILE="gcc-${GCC_VER}.tar.bz2"
+curl "ftp://gcc.gnu.org/pub/gcc/releases/gcc-${GCC_VER}/${FILE}" -o "$FILE"
+tar -xf "$FILE"
+rm "$FILE"
+mkdir "gcc-${GCC_VER}/build"
+cd "gcc-${GCC_VER}/build"
+../configure --prefix="$TOOLCHAIN_DIR" --enable-languages=c,c++ --with-gmp="$TOOLCHAIN_DIR" --with-mpfr="$TOOLCHAIN_DIR" --with-isl="$TOOLCHAIN_DIR" --with-mpc="$TOOLCHAIN_DIR" --disable-werror --disable-multilib
+make bootstrap
+make install
+cd ../../
+
+echo "Building OpenSSL"
+FILE="openssl-${OPENSSL_VER}.tar.gz"
+curl "https://www.openssl.org/source/${FILE}" -o "$FILE"
+tar -xf "$FILE"
+rm "$FILE"
+cd "openssl-${OPENSSL_VER}"
+./config no-shared no-ssl2 no-ssl3 no-idea no-dtls1 no-npn no-psk no-srp no-ec2m no-weak-ssl-ciphers no-camellia no-cmac no-des no-dh no-dsa no-dtls no-ec no-ecdh no-ecdsa no-ec-nistp-64-gcc-128 no-engine no-heartbeats no-krb5 no-md2 no-md4 no-mdc2 no-whirlpool no-x509-verify no-comp -fPIC -I/usr/include --prefix="$TOOLCHAIN_DIR"
+make depend
+make install
+cd ..
