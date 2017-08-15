266991d8f5d8e8bca76bf16ebf9f47a902b1803c
David Christenson <mail@noctem.xyz>
2017-08-14 23:08:50 -0600

    Update GCC, binutils, GMP, MPFR, and ISL
    Update GCC to 7.2.0, binutils to 2.29, GMP to 6.1.2, MPFR to 3.1.5, ISL
    to 0.18; remove build directories.

--- linux-build.sh
+++ linux-build.sh
@@ -6,14 +6,12 @@ if [[ -z "$TOOLCHAIN_DIR" ]]; then
 	TOOLCHAIN_DIR=/toolchain
 fi
 
-INFRASTRUCTURE=ftp://gcc.gnu.org/pub/gcc/infrastructure/
-
-GMP_VER=6.1.0
-ISL_VER=0.16.1
+GMP_VER=6.1.2
+MPFR_VER=3.1.5
 MPC_VER=1.0.3
-MPFR_VER=3.1.4
-GCC_VER=7.1.0
-BINUTILS_VER=2.28
+ISL_VER=0.18
+GCC_VER=7.2.0
+BINUTILS_VER=2.29
 
 mkdir -p "$TOOLCHAIN_DIR"
 
@@ -33,7 +31,7 @@ fi
 
 echo "Building GMP"
 FILE="gmp-${GMP_VER}.tar.bz2"
-curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+curl "https://gmplib.org/download/gmp/${FILE}" -o "$FILE"
 tar -xf "$FILE"
 rm "$FILE"
 cd "gmp-${GMP_VER}"
@@ -42,10 +40,11 @@ make
 make check
 make install
 cd ..
+rm -r "gmp-${GMP_VER}"
 
 echo "Building MPFR"
 FILE="mpfr-${MPFR_VER}.tar.bz2"
-curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+curl "https://ftp.gnu.org/gnu/mpfr/${FILE}" -o "$FILE"
 tar -xf "$FILE"
 rm "$FILE"
 cd "mpfr-${MPFR_VER}"
@@ -54,10 +53,11 @@ make
 make check
 make install
 cd ..
+rm -r "mpfr-${MPFR_VER}"
 
 echo "Building MPC"
 FILE="mpc-${MPC_VER}.tar.gz"
-curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+curl "https://ftp.gnu.org/gnu/mpc/${FILE}" -o "$FILE"
 tar -xf "$FILE"
 rm "$FILE"
 cd "mpc-${MPC_VER}"
@@ -66,21 +66,23 @@ make
 make check
 make install
 cd ..
-
+rm -r "mpc-${MPC_VER}"
 
 echo "Building ISL"
 FILE="isl-${ISL_VER}.tar.bz2"
-curl "${INFRASTRUCTURE}${FILE}" -o "$FILE"
+curl "http://isl.gforge.inria.fr/${FILE}" -o "$FILE"
 tar -xf "$FILE"
 rm "$FILE"
 cd "isl-${ISL_VER}"
 ./configure --disable-dependency-tracking --disable-silent-rules --prefix="$TOOLCHAIN_DIR" --with-gmp=system --with-gmp-prefix="$TOOLCHAIN_DIR"
 make check
 make install
+cd ..
+rm -r "isl-${ISL_VER}"
 
 echo "Building GCC"
-FILE="gcc-${GCC_VER}.tar.bz2"
-curl "ftp://gcc.gnu.org/pub/gcc/releases/gcc-${GCC_VER}/${FILE}" -o "$FILE"
+FILE="gcc-${GCC_VER}.tar.gz"
+curl "https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/${FILE}" -o "$FILE"
 tar -xf "$FILE"
 rm "$FILE"
 mkdir "gcc-${GCC_VER}/build"
@@ -88,6 +90,8 @@ cd "gcc-${GCC_VER}/build"
 ../configure --prefix="$TOOLCHAIN_DIR" --enable-languages=c,c++ --with-gmp="$TOOLCHAIN_DIR" --with-mpfr="$TOOLCHAIN_DIR" --with-isl="$TOOLCHAIN_DIR" --with-mpc="$TOOLCHAIN_DIR" --disable-werror --disable-multilib
 make bootstrap
 make install
+cd ../..
+rm -r "gcc-${GCC_VER}"
 
 echo "Building binutils"
 FILE="binutils-${BINUTILS_VER}.tar.bz2"
@@ -97,3 +101,5 @@ cd "binutils-${BINUTILS_VER}"
 ./configure --disable-debug --disable-dependency-tracking --prefix="$TOOLCHAIN_DIR" --with-gmp="$TOOLCHAIN_DIR" --with-mpfr="$TOOLCHAIN_DIR" --with-isl="${TOOLCHAIN_DIR}" --with-mpc="$TOOLCHAIN_DIR"
 make
 make install
+cd ..
+rm -r "binutils-${BINUTILS_VER}"
